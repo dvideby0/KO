60e2fdeb406a4c36d8333f30dec283eeaa58a971
rbrookfield <rbrookfield@yahoo.com>
2013-07-07 03:13:16 -0400

    Removed inline styles; added alerts; removed closed registration

--- Server/ko.js
+++ Server/ko.js
@@ -5,6 +5,16 @@
  * Time: 12:28 AM
  */
 
+var mongodb = require('mongodb');
+var server = new mongodb.Server("127.0.0.1", 27017, {});
+var registration;
+new mongodb.Db('ko2013', server, {w:1}).open(function (error, client) {
+    if (error) throw error;
+    registration = new mongodb.Collection(client, 'registration');
+});
+
+
+
 var express = require('express');
 var app = express();
 app.use(express.bodyParser());
@@ -21,8 +31,17 @@ app.all('*', function(req, res, next){
 });
 
 app.post('/', function(req, res){
-    console.log(req.body);
-    res.send(200);
+    registration.findOne({email: req.body.email}, function(err, post){
+        if(!post){
+            registration.insert(req.body, function(){
+                console.log('A User Has Registered...');
+                res.send(200, 'Registration Successful!');
+            });
+        }
+        else{
+            res.send(200, 'User Already Exists!');
+        }
+    });
 });
 
 app.listen(8867);
